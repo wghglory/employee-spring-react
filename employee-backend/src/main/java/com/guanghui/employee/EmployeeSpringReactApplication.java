06d59606ba63b188067adb995b64a721ae18bfa3
Guanghui Wang <guanghui-wang@foxmail.com>
2022-05-09 15:37:53 +0800

    feat: init project

---
+++ employee-backend/src/main/java/com/guanghui/employee/EmployeeSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.guanghui.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpringReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSpringReactApplication.class, args);
+    }
+
+}
---
+++ employee-backend/src/test/java/com/guanghui/employee/EmployeeSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guanghui.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSpringReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
